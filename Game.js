a3c35c9ef759432fcb29f1c60c2b173f0d0722ef
Gurjinder Massan <gurjinder.massan@gmail.com>
2019-03-17 13:57:18 +0000

    This commit updates html and displays a user choice on click

--- Game.js
+++ Game.js
@@ -1,6 +1,8 @@
 
-/* Arrays that you are going to append attributes to (such as '.element' cannot be strings, they must be objects*/
+var choicesElement = document.querySelector('#choices');
+var resultElement = document.querySelector('#result');
 
+/* Arrays that you are going to append attributes to (such as '.element') cannot be strings, they must be objects*/
 var images = [
     {
         path: "Images/bulbasaur.png"
@@ -13,19 +15,27 @@ var images = [
     }
 ];
 
-/*
-pokemon = document.createElement('img');
-pokemon.style.width = '150px';
-document.querySelector('#choices').appendChild(pokemon);
-pokemon.src=images;
-*/
-
-images.forEach(function(image, index) 
+images.forEach(function(pokemon, index) 
 {
-    image.element = document.createElement('img');
-    image.src = images[index].path;
-    image.style.width = '150px';
-    document.querySelector('#choices').appendChild(image.element);
+    pokemon.element = document.createElement('img');
+    pokemon.element.src = pokemon.path;
+    pokemon.element.style.width = '150px';
+    document.querySelector('#choices').appendChild(pokemon.element);
+    pokemon.element.addEventListener('click',function() {
+        displayResult(index);
+    });
     
 });
+
+function displayResult(userChoice) {
+    choicesElement.style.display = 'none'; 
+    resultElement.style.display = 'block';
+    
+    var userImg = document.createElement('img');
+    userImg.src = images[userChoice].path;
+    userImg.style.width = '150px';
+    resultElement.appendChild(userImg);
+    
+    
+}
     
